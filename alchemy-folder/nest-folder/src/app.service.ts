35f5aca670d0eec2266b2be1675a588ce2290ab7
linuskelsey <alfred.linus.kelsey@gmail.com>
2023-08-26 15:15:04 +0100

    Initialise app.controller and app.service

--- alchemy-folder/nest-folder/src/app.service.ts
+++ alchemy-folder/nest-folder/src/app.service.ts
@@ -1,7 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { ethers } from 'ethers';
+import * as tokenJson from '../../backend/artifacts/contracts/LotteryToken.sol/LotteryToken.json'
+import * as lotteryJson from '../../backend/artifacts/contracts/Lottery.sol/Lottery.json'
+import 'dotenv/config';
+require('dotenv').config();
 
 @Injectable()
 export class AppService {
+  tokenContract: ethers.Contract;
+  lotteryContract: ethers.Contract;
+  provider: ethers.Provider;
+  wallet: ethers.Wallet;
+
+  constructor() {
+    this.provider = new ethers.JsonRpcProvider(
+      process.env.RPC_ENDPOINT_URL ?? '',
+    );
+    this.wallet = new ethers.Wallet(
+      process.env.PRIVATE_KEY ?? '', 
+      this.provider,
+    );
+    this.tokenContract = new ethers.Contract(
+      process.env.TOKEN_ADDRESS,
+      tokenJson.abi,
+      this.wallet,
+    );
+    this.lotteryContract = new ethers.Contract(
+      process.env.LOTTERY_ADDRESS,
+      lotteryJson.abi,
+      this.wallet,
+    );
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
