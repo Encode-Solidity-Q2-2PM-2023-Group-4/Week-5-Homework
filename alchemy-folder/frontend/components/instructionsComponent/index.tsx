8910c13d33c341f081ac633e99a279aeeecd8c29
linuskelsey <alfred.linus.kelsey@gmail.com>
2023-08-26 18:30:35 +0100

    added info to index page and sepolia chain

--- alchemy-folder/frontend/components/instructionsComponent/index.tsx
+++ alchemy-folder/frontend/components/instructionsComponent/index.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import styles from "./instructionsComponent.module.css";
-import { useAccount } from "wagmi";
+import { useAccount, useBalance, useContractRead, useNetwork } from "wagmi";
+import * as tokenJson from '../../../backend/artifacts/contracts/LotteryToken.sol/LotteryToken.json';
+import * as lotteryJson from '../../../backend/artifacts/contracts/Lottery.sol/Lottery.json';
+import 'dotenv/config';
+require('dotenv').config();
 
 export default function InstructionsComponent() {
   return (
@@ -13,26 +17,97 @@ export default function InstructionsComponent() {
           <h3></h3>
         </div>
       </header>
-
-      <div className={styles.buttons_container}>
-        <div className={styles.button}>
-          <p>Add Components</p>
-        </div>
-        <div className={styles.button}>
-          <p>Explore Templates</p>
-        </div>
-        <div className={styles.button}>
-          <p>Visit Docs</p>
-        </div>
-        <div className={styles.button}>
-          <p>Contribute</p>
-        </div>
-      </div>
+      <WalletInfo></WalletInfo>
+      <PageBody></PageBody>
     </div>
   );
 }
 
-function displayOwnerPool() {
+function PageBody() {
+  return (
+    <div>
+      <CheckState></CheckState>
+      <DisplayOwnerPool></DisplayOwnerPool>
+    </div>
+  )
+}
+
+function WalletInfo() {
+  const { address, isConnecting, isDisconnected } = useAccount();
+  const { chain } = useNetwork();
+  if (address)
+    return (
+      <div>
+        <p>Your account address is <b>{address}</b>.</p>
+        <p>Connected to the <b>{chain?.name}</b> network.</p>
+        <WalletBalance address={address}></WalletBalance>
+        <TokenName></TokenName>
+        <TokenBalance address={address}></TokenBalance>
+      </div>
+    );
+  if (isConnecting)
+    return (
+      <div>
+        <p>Connecting wallet...</p>
+      </div>
+    );
+  if (isDisconnected)
+    return (
+      <div>
+        <p>Wallet disconnected. Connect wallet to continue.</p>
+      </div>
+    );
+  return (
+    <div>
+      <p>Connect wallet to continue.</p>
+    </div>
+  );
+}
+
+function WalletBalance(params: { address: `0x${string}` }) {
+  const { data, isError, isLoading } = useBalance({
+    address: params.address,
+  });
+
+  if (isLoading) return <p>Fetching balance…</p>;
+  if (isError) return <p>Error fetching balance.</p>;
+  return (
+    <div>
+      <p>Balance: <b>{data?.formatted} {data?.symbol}</b></p>
+    </div>
+  );
+}
+
+function TokenName() {
+  const { data, isError, isLoading } = useContractRead({
+    address: "0xb498F8ADcf2ca6b131b87459c0449c4Bb3De23A7",
+    abi: tokenJson.abi,
+    functionName: "name",
+  });
+
+  const name = typeof data === "string" ? data : 0;
+
+  if (isLoading) return <p>Fetching name…</p>;
+  if (isError) return <p>Error fetching name.</p>;
+  return <p>Token name: <b>{name}</b></p>;
+}
+
+function TokenBalance(params: { address: `0x${string}` }) {
+  const { data, isError, isLoading } = useContractRead({
+    address: "0xb498F8ADcf2ca6b131b87459c0449c4Bb3De23A7",
+    abi: tokenJson.abi,
+    functionName: "balanceOf",
+    args: [params.address],
+  });
+
+  const balance = typeof data === "bigint" ? data : 0;
+
+  if (isLoading) return <p>Fetching balance...</p>;
+  if (isError) return <p>Error fetching balance.</p>;
+  return <p>Token balance: <b>{Number(balance)}</b> decimal units of <b>LotteryToken</b>.</p>;
+}
+
+function DisplayOwnerPool() {
   const [data, setData] = useState<any>(null);
   const [isLoading, setLoading] = useState(true);
   const {isConnected, isDisconnected} = useAccount();
@@ -56,7 +131,7 @@ function displayOwnerPool() {
   );
 }
 
-function checkState() {
+function CheckState() {
   const [data, setData] = useState<any>(null);
   const [isLoading, setLoading] = useState(true);
   const {isConnected, isDisconnected} = useAccount();
