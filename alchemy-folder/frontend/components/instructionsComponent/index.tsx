8984f93f671b3ee7a1326debd69a16ec5a05e995
Adam Czopp <acz@duck.com>
2023-08-27 22:31:56 +0100

    Update index.tsx

--- alchemy-folder/frontend/components/instructionsComponent/index.tsx
+++ alchemy-folder/frontend/components/instructionsComponent/index.tsx
@@ -156,7 +156,7 @@ function CheckState() {
   );
 }
 
-function BuyTokens(params: {address: string}) {
+function BuyTokens(params: {value: number}) {
   const { config } = usePrepareSendTransaction();
   const { data, isLoading, isSuccess } = useSendTransaction(config);
   const [amount, setAmount] = useState("");
@@ -166,7 +166,7 @@ function BuyTokens(params: {address: string}) {
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ address: params.address, amount: amount })
+    body: JSON.stringify({ amount: amount })
   };
 
   if (!data) return (
@@ -176,7 +176,7 @@ function BuyTokens(params: {address: string}) {
           Enter amount of tokens to purchase:
           <input
             type="number"
-            value={amount}
+            value={value}
             onChange={(e) => setAmount(e.target.value)} 
           />
         </label>
@@ -193,7 +193,7 @@ function BuyTokens(params: {address: string}) {
   return <></>
 }
 
-function WithdrawTokens(params: { address: string }) {
+function WithdrawTokens(params: { amount: number }) {
   const { config } = usePrepareSendTransaction();
   const { data, isLoading, isSuccess } = useSendTransaction(config);
   const [amount, setAmount] = useState("");
@@ -203,7 +203,7 @@ function WithdrawTokens(params: { address: string }) {
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ address: params.address, amount: amount })
+    body: JSON.stringify({ amount: amount })
   };
 
   if (!data) return (
